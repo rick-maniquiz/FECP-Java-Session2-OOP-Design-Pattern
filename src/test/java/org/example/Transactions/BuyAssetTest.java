e7063cc1ce077b5ac7b413bf5efa1ce87b4eedea
Adrian Garett Sian <adriansian@gmail.com>
2025-07-04 13:05:31 +0800

    unit-testing: add required classes for unit-testing
    Signed-off-by: Adrian Garett Sian <adriansian@gmail.com>

---
+++ src/test/java/org/example/Transactions/BuyAssetTest.java
@@ -0,0 +1,18 @@
+package org.example.Transactions;
+
+import org.example.DigitalAsset;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuyAssetTest {
+    private BuyAsset buyAsset;
+    private ArrayList<DigitalAsset> walletAssets;
+    private DigitalAsset cryptoAsset;
+    private DigitalAsset cashAsset;
+
+
+    
+
+}
